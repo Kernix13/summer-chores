b8d9d290297224dd03a26343195bb701d2038363
Jim <jimkernicky@gmail.com>
2025-10-03 07:31:35 -0400

    add promiseVersion.js

---
+++ promiseVersion.js
@@ -0,0 +1,91 @@
+function mowYard(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 10);
+      if (randomNumber < 5) {
+        resolve(`${name} mowed the yard.`);
+      } else {
+        reject(`${name} fell asleep after mowing the yard.`);
+      }
+    }, 2000);
+  });
+}
+
+function weedEat(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 10);
+      if (randomNumber) {
+        resolve(`${name} finished using the weed eater.`);
+      } else {
+        reject(`${name} fell asleep after weed eating the yard.`);
+      }
+    }, 1500);
+
+  })
+}
+
+function trimHedges(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 10);
+      if (randomNumber) {
+        resolve(`${name} finished trimming the hedges.`);
+      } else {
+        reject(`${name} fell asleep after trimming the hedges.`);
+      }
+    }, 1000);
+
+  })
+}
+
+function collectWood(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 10);
+      if (randomNumber) {
+        resolve(`${name} finished collecting wood.`);
+      } else {
+        reject(`${name} fell asleep after collecting wood.`);
+      }
+    }, 2500);
+
+  })
+}
+
+function waterGarden(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 10);
+      if (randomNumber) {
+        resolve(`${name} finished watering the garden.`);
+      } else {
+        reject(`${name} fell asleep after watering the garden.`);
+      }
+    }, 500);
+
+  })
+}
+
+mowYard('Samwise')
+  .then(value => {
+    console.log(value);
+    return weedEat('Samwise');
+  })
+  .then(value => {
+    console.log(value);
+    return trimHedges('Samwise');
+  })
+  .then(value => {
+    console.log(value);
+    return collectWood('Samwise');
+  })
+  .then(value => {
+    console.log(value);
+    return waterGarden('Samwise');
+  })
+  .then(value => {
+    console.log(value);
+    console.log(`Samwise finished all their chores!`);
+  })
+  .catch(err => console.error(err))
